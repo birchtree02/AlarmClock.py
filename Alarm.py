22dee40b23ac410e0be175c6a84f5e04f4b8ba96
birchtree02 <edbirchy664+gh@gmail.com>
2016-06-01 12:18:35 +0100

    Add files via upload

---
+++ Alarm.py
@@ -0,0 +1,13 @@
+﻿# alarm
+
+from time import gmtime, strftime
+
+def alarm():
+	while True:
+		if strftime("%H:%M", gmtime()) == alarmtime:
+			print('wake up')
+			print('\007')
+			break
+
+alarmtime = input('When would you like to be woken up? (hh:mm)
+alarm()
